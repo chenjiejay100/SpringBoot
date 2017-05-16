67a2e786291ae03281ca728017d1b88090d45e16
chenjiejay100 <chenjiejay100@outlook.com>
2017-05-16 15:19:17 +0800

    add

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,21 @@
+package com.example;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/happy")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/happy/Application.java
@@ -0,0 +1,20 @@
+package happy;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableAutoConfiguration
+public class Application {
+	
+    @RequestMapping("/hello")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
